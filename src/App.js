d91ddccd8599bce87563b405a6d531c1f73411af
Dmitriy.Aleksandrov <Dmitriy.Aleksandrov@tatar.ru>
2021-10-27 22:08:45 +0300

    First component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './App.css';
+import Header from './components/Header';
 
 function App() {
-    return <div className='App'>Hello world</div>;
+    return <Header />;
 }
 
 export default App;
