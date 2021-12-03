efa2e124297d46bfdb04e7e92a97f596382958f7
Dmitriy.Aleksandrov <Dmitriy.Aleksandrov@tatar.ru>
2021-12-03 13:52:27 +0300

    view-note | html in text

--- src/components/NoteViewMode/Blocks/Text/index.jsx
+++ src/components/NoteViewMode/Blocks/Text/index.jsx
@@ -1,7 +1,7 @@
 import styles from './style.module.css';
 
 const TextBlock = (props) => {
-    return <div className={styles.textBlock}>{props.text}</div>;
+    return <div className={styles.textBlock} dangerouslySetInnerHTML={{ __html: props.text }} />;
 };
 
 export default TextBlock;
