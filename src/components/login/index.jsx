4d4d9455acdbdb54ef267b311216dd8e122d8f5c
AnnaAleynik <anna.mikhailovna305@gmail.com>
2021-12-16 00:38:33 +0300

    add

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,16 +1,16 @@
-import { auth, provider } from '../../service/firebase';
+import { auth, provider, database, queryGetUsers, newUserKey, saveNewUser } from '../../service/firebase';
 import { setUser } from '../../store/actions';
 
 import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
 
-const userList = [
-    {
-        id: 1,
-        email: 'deadm2249@gmail.com',
-        name: 'Dead M',
-    },
-];
+// const userList = [
+//     {
+//         id: 1,
+//         email: 'deadm2249@gmail.com',
+//         name: 'Dead M',
+//     },
+// ];
 
 const signInWithGoogle = (onSuccess) => {
     auth.signInWithPopup(provider)
@@ -26,17 +26,38 @@ const Login = () => {
     const dispatch = useDispatch();
 
     const onSuccess = (result) => {
-        const { email } = result.user;
-        const foundedUser = userList.find((u) => u.email === email);
-        let user;
-        if (foundedUser === null) {
-            // write to database id email name
-            // user = foundedUser;
-            // get user id
-        } else {
-            user = foundedUser;
-        }
-        dispatch(setUser(user));
+        const { email, displayName } = result.user;
+
+        // const userList = getUsers();
+        queryGetUsers
+            .then((snapshot) => {
+                if (snapshot.exists()) {
+                    console.log(snapshot.val());
+                    // snapshot.val();
+                    const userList = snapshot.val();
+                    // don't work
+                    const foundedUser = userList.find((u) => u.email === email);
+                    let user;
+                    if (foundedUser === undefined) {
+                        const userId = newUserKey;
+                        console.log(newUserKey);
+                        user = {
+                            id: userId,
+                            name: displayName,
+                            email: email,
+                        };
+                        saveNewUser(user);
+                    } else {
+                        user = foundedUser;
+                    }
+                    dispatch(setUser(user));
+                } else {
+                    console.log('No data available');
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     };
 
     return (
