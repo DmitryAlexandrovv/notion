b3ca05e980617553cfb68c497665cf15a98cc6ae
Dmitriy Alexandrov <diman2190@gmail.com>
2021-12-16 21:27:40 +0300

    redux-persist | сохраняем store в localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
 import { createStore } from 'redux';
 import reducer from '../reducers';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-const store = createStore(reducer);
+const persistConfig = {
+    key: 'root',
+    storage,
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+export { store, persistor };
