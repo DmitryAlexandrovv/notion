7825637170e159df86e5271843f9195672b402aa
Dmitry Alexandrov <57902455+DmitryAlexandrovv@users.noreply.github.com>
2021-12-23 10:51:19 +0300

    UI (#39)
    * user-icon styles fix
    
    * responsive ui

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
 import firebaseService from '../../service/firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { clearStore, setLoading } from '../../store/actions';
+import { TreeSelect } from 'antd';
+import { createNoteTree, getNestedArray } from '../../helpers';
+import { useNavigate } from 'react-router-dom';
+import UserData from '../user-data';
 
 import styles from './style.module.css';
 
 const Navbar = () => {
-    const dispatch = useDispatch();
+    const dispatch = useDispatch(),
+        navigate = useNavigate(),
+        pages = useSelector((state) => state.pages),
+        formattedPages = getNestedArray(pages, undefined);
 
     const logout = (event) => {
         dispatch(setLoading(true));
@@ -24,9 +31,17 @@ const Navbar = () => {
             });
     };
 
+    const openNote = (pageId) => {
+        const page = pages[Object.keys(pages).find((key) => key === pageId)];
+        navigate('/note/' + (page.url ? page.url : pageId), { replace: true });
+    };
+
     return (
         <div>
             <ul className={styles.navbar}>
+                <li className={styles.navbarUserItem}>
+                    <UserData />
+                </li>
                 <li>
                     <Link to='/' className={styles.navbarLink}>
                         Home
@@ -36,6 +51,13 @@ const Navbar = () => {
                     <Link to='/logout' className={styles.navbarLink} onClick={logout}>
                         Logout
                     </Link>
+                </li>
+                <li className={styles.navbarTreeItem}>
+                    <TreeSelect value={undefined} onChange={(id) => openNote(id)} className={styles.noteTreeLink}>
+                        {formattedPages.map((page) => {
+                            return createNoteTree(page);
+                        })}
+                    </TreeSelect>
                 </li>
             </ul>
         </div>
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,26 +1,13 @@
 import PagesList from '../pages-list';
-import { useSelector } from 'react-redux';
+import UserData from '../user-data';
 
 import styles from './style.module.css';
 
-const UserData = ({ user }) => {
-    const { name } = user;
-
-    return (
-        <div className={styles.user}>
-            <span className={styles.userIcon}>A</span>
-            <span className={styles.userName}>{name}</span>
-        </div>
-    );
-};
-
 const Sidebar = () => {
-    const user = useSelector((state) => state.user);
-
     return (
         <aside className={styles.sidebar}>
             <div className={styles.wrapper}>
-                <UserData user={user} />
+                <UserData />
                 <PagesList />
             </div>
         </aside>
